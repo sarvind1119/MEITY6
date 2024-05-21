ad983bbdbf49a8879f105701a96560811c4ba34f
sarvind1119 <69563525+sarvind1119@users.noreply.github.com>
2024-05-21 15:42:36 +0530

    Update app.py
    changed index name from meity1 to meity2 which has PLI scheme and national AI mission doc as well

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ region = 'us-east-1'
 
 spec = ServerlessSpec(cloud=cloud, region=region)
 
-index_name = 'meity1'
+index_name = 'meity2'
 
 # get openai api key from platform.openai.com
 OPENAI_API_KEY =  os.environ.get('OPENAI_API_KEY')
